643264aec8885568ffc3e1132fd2cbaae7f93884
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-14 10:12:30 -0300

    Feat - show endpoint shcedules created

--- src/controllers/schedules.controller.ts
+++ src/controllers/schedules.controller.ts
@@ -32,9 +32,28 @@ class SchedulesController {
     try{
         const schedule = await db.schedules.findMany()
         if(!schedule){
-            return res.status(400).json({ success: false, msg: "Sheldule not listed" })
+            return res.status(400).json({ success: false, msg: "Shedules not listed" })
         }
-        return res.status(200).json({ success: true, msg:"Scheldul listing", data: schedule })
+        return res.status(200).json({ success: true, msg:"Schedules listing", data: schedule })
+    } catch(err){
+        return res.status(500).json({ success: false, msg: "Error database" })
+    }
+  }
+
+  public async show(req: Request, res: Response){
+    const { id } = req.params
+    try{
+        const schedule = await db.schedules.findUnique({
+            where:{
+                id
+            }
+        })
+
+        if(!schedule){
+            return res.status(404).json({ success: false, msg: "Schedule not found" })
+        }
+
+        return res.status(200).json({ sucess: true, msg: "Schedule showing", data: schedule })
     } catch(err){
         return res.status(500).json({ success: false, msg: "Error database" })
     }
