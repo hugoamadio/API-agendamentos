fa1114c35ab4929a3135e089c3b59c5bbc23d223
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-15 08:55:50 -0300

    Feat - install and configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import schedulesRoutes from './routes/schedules.routes'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
